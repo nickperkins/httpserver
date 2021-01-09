4d98ab4b787be7b4df02ccc988c8365db02359b7
Nick Perkins <n.perkins@arkoselabs.com>
2021-01-09 13:39:22 +1000

    Add blind timer

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -19,6 +19,6 @@ func main() {
 	defer closeStore()
 	fmt.Println("Let's play poker")
 	fmt.Println("Type {Name} wins to record a win")
-	poker.NewCLI(store, os.Stdin).PlayPoker()
+	poker.NewCLI(store, os.Stdin, poker.BlindAlerterFunc(poker.StdOutAlerter)).PlayPoker()
 
 }
